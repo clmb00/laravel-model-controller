889509772549f82d306c27e82b8faf137872d3aa
clmb00 <paolo.colombo2000@gmail.com>
2023-01-09 16:15:25 +0100

    [add] error 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.layout')
+
+@section('title')
+    Error 404
+@endsection
+
+@section('content')
+    <div class="bg-danger p-3 my-3 mx-5 ps-4 rounded-5 text-white text-center">
+        <h5 class="fw-bolder">Error 404!</h5>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque id, veniam, voluptas atque nisi distinctio rerum fugiat asperiores fugit at blanditiis quam inventore suscipit tempora nostrum beatae deserunt quos aut.</p>
+    </div>
+@endsection

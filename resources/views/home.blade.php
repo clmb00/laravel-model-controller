74670325c1bdee592c2b46ab34247703a9c03d0a
clmb00 <paolo.colombo2000@gmail.com>
2023-01-09 15:10:14 +0100

    start

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.layout');
+
+@section('content')
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates explicabo odio numquam odit quisquam placeat distinctio quod dicta. Quos, voluptas. Facere debitis quaerat ipsa repellat ex! Possimus dolores sit velit.</p>
+@endsection
